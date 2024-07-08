159c2a923b28272742e6eb2b7e214afdf0b2887b
brunomokbel <bruno.mokbel@outlook.com>
2024-07-08 13:55:58 +0200

    structure

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__, template_folder='venv/templates')
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Render the sign-up page
+@app.route('/')
+def signup_form():
+    return render_template('sign-up.html')
 
+# Handle the form submission
+@app.route('/signup', methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+    # Here you would typically save the user data to a database
+    # For this example, we'll just return the data as JSON
+    return jsonify({"message": "User signed up successfully!", "username": username})
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+# Run the app
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True, port = 5000)
